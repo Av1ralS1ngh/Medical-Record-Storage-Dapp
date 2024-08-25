7eccfe57c5abf18006c0b65d1ab5acd384459344
Arnav Kirti <arnav.tkd@gmail.com>
2024-08-25 17:23:29 +0530

    added function addRecord

--- medicalrecord/contracts/MedicalRecord.sol
+++ medicalrecord/contracts/MedicalRecord.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.22;
 
 contract MedicalRecord {
     uint public id;
-    mapping(uint=>Record) records;
-    mapping(uint=>bool) public isDeleted;
+    mapping(uint => Record) records;
+    mapping(uint => bool) public isDeleted;
     struct Record {
         uint id;
         uint timestamp;
@@ -15,5 +15,51 @@ contract MedicalRecord {
         string allergies;
         string diagnosis;
         string treatment;
+    }
+
+    event MedicalRecord__addRecord(
+        uint id,
+        uint timestamp,
+        string name,
+        uint age,
+        string gender,
+        string bloodType,
+        string allergies,
+        string diagnosis,
+        string treatment
+    );
+
+    function addRecord(
+        string memory _name,
+        uint _age,
+        string memory _gender,
+        string memory _bloodType,
+        string memory _allergies,
+        string memory _diagnosis,
+        string memory _treatment
+    ) public {
+        id++;
+        records[id] = Record(
+            id,
+            block.timestamp,
+            _name,
+            _age,
+            _gender,
+            _bloodType,
+            _allergies,
+            _diagnosis,
+            _treatment
+        );
+        emit MedicalRecord__addRecord(
+            id,
+            block.timestamp,
+            _name,
+            _age,
+            _gender,
+            _bloodType,
+            _allergies,
+            _diagnosis,
+            _treatment
+        );
     }
 }
