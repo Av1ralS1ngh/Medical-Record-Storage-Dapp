73482174a4d71871d31e3dc01e3ccbe5a491731a
Aviral Singh <aviral_s@mt.iitr.ac.in>
2024-08-26 06:59:17 +0530

    Added Complete Backend

--- medicalrecord/hardhat.config.js
+++ medicalrecord/hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const privateKeys = process.env.PRIVATE_KEYS || "";
+const goerliApiKey = process.env.GOERLI_API_KEY || "";
+const mumbaiApiKey = process.env.MUMBAI_API_KEY || "";
+
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.18",
+  networks: {
+    localhost: {},
+    goerli: {
+      url: https://${goerliApiKey}, // Ensure the URL is complete
+      accounts: privateKeys ? privateKeys.split(",") : [],
+    },
+    mumbai: {
+      url: https://${mumbaiApiKey}, // Ensure the URL is complete
+      accounts: privateKeys ? privateKeys.split(",") : [],
+    },
+  },
 };
